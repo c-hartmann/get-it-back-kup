e461d93ecdb05dd9dbb0a3adae680cc69d030c26
Christian Hartmann <hartmann.christian@gmail.com>
2025-02-26 07:13:44 +0100

    fix new options

--- get-it-back-kup.sh
+++ get-it-back-kup.sh
@@ -18,8 +18,9 @@ function usage ()
 kuprc_file_path="$HOME/.config/kuprc"
 
 ### bup restores to current directory by default
-# option_out_dir='/no/dir/no/where/existing'
-option_out_dir="$(mktemp --dry-run --directory)"
+# out_dir_option='/no/dir/no/where/existing'
+out_dir_default="$(mktemp --dry-run --directory)"
+out_dir_option="$out_dir_default" # required later to get out dir for every(!) file individualy
 
 ### by default we do!
 dry_run=false
@@ -38,7 +39,7 @@ while true; do
 			shift 2
 		;;
 		-C | --outdir )
-			option_out_dir="$2"
+			out_dir_option="$2"
 			shift 2
 		;;
 		-D | --dry-run )
@@ -62,8 +63,8 @@ while true; do
 done
 
 ### no file to retore, just quit with usage
-file_to_restore_path="${1:-/nopath}"
-[[ "$file_to_restore_path" == '/nopath' ]] && usage && exit 2
+file_restore_path="${1:-/nopath}"
+[[ "$file_restore_path" == '/nopath' ]] && usage && exit 2
 
 ### get kreadconfig command installed here
 kreadconfig_commands_array=( $(type -p kreadconfig6 kreadconfig5 kreadconfig) )
@@ -132,81 +133,83 @@ function get_external_drive_mount_path ()
 kup_plan_count=$(grep 'Paths included=' "$kuprc_file_path" | wc -l)
 
 ### loop over files given
-for file_to_restore_path in "${@}"; do
+for file_restore_path in "${@}"; do
 
-### we restore to original directory by default (if not given as option)
-[[ "$option_out_dir" == '/no/dir/no/where/existing' ]] && out_dir="$(dirname "$file_to_restore_path")" || out_dir="$option_out_dir"
+	### we restore to original directory by default (if not given as option)
+	[[ "$out_dir_option" == "$out_dir_default" ]] && out_dir="$(dirname "$file_restore_path")" || out_dir="$out_dir_option"
 
-# TODO: add even more error resistance
-for (( i = 0 ; i < $kup_plan_count ; i++ )); do
-	echo >&2 # just some visual separation
-	plan_num=$(( i+1 ))
-	echo "plan group: [Plan/$plan_num]" >&2
-	path_incluced="$(get_key_from_config "Plan/$plan_num" 'Paths included')"
+	# TODO: add even more error resistance
+	for (( i = 0 ; i < $kup_plan_count ; i++ )); do
+		echo >&2 # just some visual separation
+		plan_num=$(( i+1 ))
+		echo "plan group: [Plan/$plan_num]" >&2
+		path_incluced="$(get_key_from_config "Plan/$plan_num" 'Paths included')"
 
-	### get some values from plan, that might be required later
-	filesystem_destination_path="$(get_key_from_config "Plan/$plan_num" 'Filesystem destination path')"
-	external_volume_label=$(get_key_from_config "Plan/$plan_num" "External volume label")
-	external_drive_destination_path=$(get_key_from_config "Plan/$plan_num" "External drive destination path")
+		### get some values from plan, that might be required later
+		filesystem_destination_path="$(get_key_from_config "Plan/$plan_num" 'Filesystem destination path')"
+		external_volume_label=$(get_key_from_config "Plan/$plan_num" "External volume label")
+		external_drive_destination_path=$(get_key_from_config "Plan/$plan_num" "External drive destination path")
 
-	### use volume label stuff only if we have no (internal/local) filesystem destination path
-	if [[ "$filesystem_destination_path"  =~ 'file://' ]]; then
-		repository_path=${filesystem_destination_path#'file://'}
-		is_kup_repository "$repository_path" && echo "valid local path: $repository_path" >&2 || echo "no valid local path: $repository_path" >&2
-	else
-		external_drive_mount_path="$(get_external_drive_mount_path "$external_volume_label")"	# hidden or
-		external_drive_mount_path="$(mount | grep "$external_volume_label" | cut -f 3 -d ' ')"	# visible?
-		if [[ -z "$external_drive_mount_path" ]]; then
-			echo "external volume label is defined, but volume not mounted: '$external_volume_label'" >&2
+		### use volume label stuff only if we have no (internal/local) filesystem destination path
+		if [[ "$filesystem_destination_path"  =~ 'file://' ]]; then
+			repository_path=${filesystem_destination_path#'file://'}
+			is_kup_repository "$repository_path" && echo "valid local path: $repository_path" >&2 || echo "no valid local path: $repository_path" >&2
 		else
-			repository_path="$external_drive_mount_path/$external_drive_destination_path"
-			is_kup_repository "$repository_path" && echo "valid external path: $repository_path" >&2 || echo "no valid external path: $repository_path" >&2
-
-			# try to get file back ...
-			# with 'kioclient ls' with this url: bup:///media/christian/MyBook/BACKUPS/Auto/Kup/solo/Entwicklung/ ?
-			# ERROR:  kf.kio.core: UDSEntry for '.' not found, creating a default one. Please fix the "kioworker" KIO worker.
-			# BUT: it returns 'kup', which is correct
-			bup_dir="$repository_path"
-
-			base_path_latest="kup/latest"
-
-			# get other pathes
-			kioclient_url="bup://$repository_path/"
-			echo "kioclient url: $kioclient_url" >&2
-			### commands pre last line is mysterious single '.' and last line is an empty one. we remove that
-			kup="$(kioclient ls "$kioclient_url" 2>/dev/null | head --lines=-2)"
-			echo "snapshots: ..." >&2
-			kioclient ls "$kioclient_url/$kup" 2>/dev/null | head --lines=-2 >&2
-			snapshots=( $(kioclient ls "$kioclient_url/$kup" 2>/dev/null | head --lines=-2) )
-
-			# do we have the file?
-			search_in_backup_path="$base_path_latest/$file_to_restore_path"
-
-			# TODO: test on entire directories
-			# TODO: is there a do-not-override option with bup?
-			echo "looking for: $search_in_backup_path …" >&2
-			found="$(bup --bup-dir="$bup_dir" ls "$search_in_backup_path" 2>/dev/null)"
-			if [[ $? = 0 ]]; then
-				echo "found: $found" >&2
-				echo "restoring to directory: $out_dir" >&2
-				### create "backup" if not disabled
-				if ! $no_save ; then
-					file_to_restore_name="$(basename "$file_to_restore_path")"
-					file_backup_path="$out_dir/${file_to_restore_name}.$today"
-					if [[ -e "$file_backup_path" ]] ; then
-						echo "creating backup to: $file_backup_path" >&2
-						cp -rp "$out_dir/$file_to_restore_name" "$file_backup_path"
-					fi
-				fi
-				echo "running command: bup --bup-dir="$bup_dir" restore --outdir="$out_dir" -v -v "$search_in_backup_path"" >&2
-				! $dry_run && bup --bup-dir="$bup_dir" restore --outdir="$out_dir" -v -v "$search_in_backup_path"
-				continue 2 # with next file to restore
+			external_drive_mount_path="$(get_external_drive_mount_path "$external_volume_label")"	# hidden or
+			external_drive_mount_path="$(mount | grep "$external_volume_label" | cut -f 3 -d ' ')"	# visible?
+			if [[ -z "$external_drive_mount_path" ]]; then
+				echo "external volume label is defined, but volume not mounted: '$external_volume_label'" >&2
 			else
-				echo "not found" >&2
+				repository_path="$external_drive_mount_path/$external_drive_destination_path"
+				is_kup_repository "$repository_path" && echo "valid external path: $repository_path" >&2 || echo "no valid external path: $repository_path" >&2
+
+				# try to get file back ...
+				# with 'kioclient ls' with this url: bup:///media/christian/MyBook/BACKUPS/Auto/Kup/solo/Entwicklung/ ?
+				# ERROR:  kf.kio.core: UDSEntry for '.' not found, creating a default one. Please fix the "kioworker" KIO worker.
+				# BUT: it returns 'kup', which is correct
+				bup_dir="$repository_path"
+
+				base_path_latest="kup/latest"
+
+				# get other pathes
+				kioclient_url="bup://$repository_path/"
+				echo "kioclient url: $kioclient_url" >&2
+				### commands pre last line is mysterious single '.' and last line is an empty one. we remove that
+				kup="$(kioclient ls "$kioclient_url" 2>/dev/null | head --lines=-2)"
+				echo "snapshots: ..." >&2
+				kioclient ls "$kioclient_url/$kup" 2>/dev/null | head --lines=-2 >&2
+				snapshots=( $(kioclient ls "$kioclient_url/$kup" 2>/dev/null | head --lines=-2) )
+
+				# do we have the file?
+				search_in_backup_path="$base_path_latest/$file_restore_path"
+
+				# TODO: test on entire directories
+				# TODO: is there a do-not-override option with bup?
+				echo "looking for: $search_in_backup_path …" >&2
+				found="$(bup --bup-dir="$bup_dir" ls "$search_in_backup_path" 2>/dev/null)"
+				if [[ $? = 0 ]]; then
+					echo "found: $found" >&2
+					echo "restoring to directory: $out_dir" >&2
+					### create "backup" if not disabled
+					set -x
+					if ! $no_save ; then
+						file_to_restore_name="$(basename "$file_restore_path")"
+						file_backup_path="$out_dir/${file_to_restore_name}.$today"
+						if [[ -e "$file_restore_path" ]] ; then
+							echo "creating backup to: $file_backup_path" >&2
+							cp -rp "$out_dir/$file_to_restore_name" "$file_backup_path"
+						fi
+					fi
+					set +x
+					echo "running command: bup --bup-dir="$bup_dir" restore --outdir="$out_dir" -v -v "$search_in_backup_path"" >&2
+					! $dry_run && bup --bup-dir="$bup_dir" restore --outdir="$out_dir" -v -v "$search_in_backup_path"
+					continue 2 # with next file to restore
+				else
+					echo "not found" >&2
+				fi
 			fi
 		fi
-	fi
-done
+	done
 
 done
 
